f5ee168819487c2ab3b4f801f4337111c1ca449b
AnnGgaa1 <94783777+AnnGgaa1@users.noreply.github.com>
2021-12-06 19:15:08 +0100

    update

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,5 +3,17 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-    return "";
+    if(command == "add"){
+        return to_string(first + second);
+        }else if(command == "subtract"){
+        return to_string(first - second);
+        }else if(command == "multiply"){
+        return to_string(first * second);
+        }else if(command == "divide"){
+        if(second != 0){
+            return to_string(first / second);}
+        else
+        {return "ERROR. Division by 0";}
+    } else {
+        return "Invalid data";};
 }
