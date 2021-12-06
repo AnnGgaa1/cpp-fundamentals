54a422f46f2f1f0febe4ad0c1c48b79c6ee2ee36
AnnGgaa1 <94783777+AnnGgaa1@users.noreply.github.com>
2021-12-06 20:57:17 +0100

    Update 01-printVector.cpp

--- 04-stl-basics/tasks/01-printVector.cpp
+++ 04-stl-basics/tasks/01-printVector.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+using namespace std;
 // Implement printVector to print the content of a given vector on screen.
 // Each string in a new line.
+string printVector(vector<string> vec){
+for(auto element:vec){
+    cout<<element<<'\n';
+}
+}
+
 
 int main() {
-    std::vector<std::string> vec {
+    vector<string> vec {
         "Hello Coders School!",
         "Welcome to the best C++ course ever",
         "Man, this is crazy :)"
